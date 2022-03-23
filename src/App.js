21572ab7cb71124472daf52c06085a95386e3954
Minjae Kim <82329983+mxx-kor@users.noreply.github.com>
2022-03-23 21:28:43 +0900

    created To Do list with React

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-
+import { useState } from "react";
 
 function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value)
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray])
+    setToDo("");
+  }
   return (
     <div>
-      <p>Hello React</p>
+      <h1>Daily To Do ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} type="text" placeholder="write your to do..." />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => <li key={index}><input type="checkbox"/>  {item}</li>)}
+      </ul>
     </div>
   );
 }
