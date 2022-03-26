ceae0e7f433d3135bbdca9d4799d37756714228f
Minjae Kim <82329983+mxx-kor@users.noreply.github.com>
2022-03-26 17:59:13 +0900

    코인 정보 API를 가져와 표시함

---
+++ src/Coin.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+
+function Coin_tracker() {
+    const[loading, setLoading] = useState(true);
+    const[coins, setCoins] = useState([]);
+    useEffect(() => {
+        fetch("https://api.coinpaprika.com/v1/tickers")
+        .then((response) => response.json())
+        .then((json) => {
+            setCoins(json);
+            setLoading(false);
+        });
+    }, [])
+    return <div>
+        <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+        {loading ? <strong>Loading...</strong> : <select>
+            {coins.map((coin) => <option>{coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD</option>)}
+        </select>}
+    </div>
+}
+// 내가 가진 돈이 얼마인지 확인해서 몇개의 비트코인이나 이더리움을 가질 수 있는지.
+
+export default Coin_tracker;
