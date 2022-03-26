77f44ceb01b460e726ad088e04dafb42c61353b0
Minjae Kim <82329983+mxx-kor@users.noreply.github.com>
2022-03-26 17:58:50 +0900

    여러가지 기능을 가진 웹앱으로 만들 계획

--- src/App.js
+++ src/App.js
@@ -1,42 +1,13 @@
 import { useState } from "react";
-import "./App.css";
+import Coin_tracker from "./Coin";
+import ToDo_list from "./Todo-list";
 
 function App() {
-  let toDoData = []
-  const savedData = () => {
-    let len = window.localStorage.length
-    if (len !== 0){
-      for (let i = len-1; i >= 0; i--)
-      toDoData.push(window.localStorage.getItem(i));
-    }
-  }
-  savedData()
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState(toDoData ? toDoData : []);
-  const onChange = (event) => setToDo(event.target.value)
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray])
-    saveToDos();
-    setToDo("");
-  }
-  const saveToDos = () => {
-    window.localStorage.setItem(toDos.length, toDo)
-  }
   return (
     <div>
-      <h1>Daily To Do ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={toDo} type="text" placeholder="write your to do..." />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => <li key={index}><input type="checkbox"/>  {item}</li>)}
-      </ul>
+      <ToDo_list />
+      <hr/>
+      <Coin_tracker/>
     </div>
   );
 }
