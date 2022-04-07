7ef50ba57997d130cb17d62f12e4db195912147d
Minjae Kim <82329983+mxx-kor@users.noreply.github.com>
2022-04-07 20:43:42 +0900

    made components

--- src/routes/GreenEffort.js
+++ src/routes/GreenEffort.js
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import '../../node_modules/react-calendar/dist/Calendar.css';
 import { createGlobalStyle } from 'styled-components';
 import NavBar from "../components/NavBar";
+import moment from 'moment';
+import styled from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
 body {
@@ -8,12 +12,42 @@ body {
 }
 `;
 
+const GeWrapper = styled.div`
+    margin: 30px auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`
+
 const GreenEffort = () => {
+    const [value, onChange] = useState(new Date());
+    const [num, setNum] = useState(0);
+
+    const plus = () => {
+        setNum(num + 1);
+    }
+    const minus = () => {
+        setNum(num - 1);
+    }
+
     return (
         <>
             <NavBar />
             <GlobalStyle />
-            <h1>GE</h1>
+            <GeWrapper>
+                <h1>Green your Effort!</h1>
+                <Calendar onChange={onChange} value={value} />
+                <div className="text-gray-500 mt-4">
+                    {moment(value).format("YYYY년 MM월 DD일")} 
+                </div>
+                <div>
+                    <button onClick={plus}>+</button>
+                    <input type='number' value={num} />
+                    <button onClick={minus}>-</button>
+                </div>
+                <button>commit!</button>
+            </GeWrapper>
         </>
     )
 }
